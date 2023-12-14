1c51d7b822c77a780c2df26b179b3bfacf02e83f
Борщев Владимир Андреевич <va.borshhev@rainvest.ru>
2023-12-14 18:30:13 +0300

    Add registerComponent

---
+++ src/utils/registerComponent.ts
@@ -0,0 +1,39 @@
+import { Block } from './Block'
+import Handlebars, { type HelperOptions } from 'handlebars'
+
+export function registerComponent (name: string, Component: typeof Block) {
+	if (name in Handlebars.helpers) {
+		throw new Error(`Компонент ${name} уже зарегистрирован`)
+	}
+
+	Handlebars.registerHelper(
+		name,
+		function (this: unknown, { hash, data, fn }: HelperOptions) {
+			const component = new Component(hash)
+
+			if ('ref' in hash) {
+				(data.root.__refs = data.root.__refs || {})[hash.ref] = component
+			}
+
+			const dataAttribute = `data-id="${component.id}"`
+			;(data.root.__children = data.root.__children || []).push({
+				component,
+				embed (fragment: DocumentFragment) {
+					const stub = fragment.querySelector(`[${dataAttribute}]`)
+
+					if (!stub) {
+						return
+					}
+
+					component.getContent()?.append(...Array.from(stub.childNodes))
+
+					stub.replaceWith(component.getContent()!)
+				},
+			})
+
+			// Если в приходит fn - это блочный хелпер
+			const contents = fn ? fn(this) : ''
+			return `<div ${dataAttribute}>${contents}</div>`
+		}
+	)
+}
