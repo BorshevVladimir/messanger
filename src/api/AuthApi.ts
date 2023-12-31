a2bd3abb1a226d54e3d310336aa5f6df976ddd25
Борщев Владимир Андреевич <va.borshhev@rainvest.ru>
2023-12-31 11:55:19 +0300

    Add AuthApi class

---
+++ src/api/AuthApi.ts
@@ -0,0 +1,55 @@
+import { HTTPTransport } from './HTTPTransport'
+
+export type SignupRequestData = {
+	first_name: string
+	second_name: string
+	login: string
+	email: string
+	password: string
+	phone: string
+}
+
+type UserId = number
+type SignupResponseData = {
+	id: UserId
+}
+
+export type SigninRequestData = {
+	login: 'string'
+	password: 'string'
+}
+
+type UserInfoResponseData = {
+	id: UserId
+	first_name: string
+	second_name: string
+	display_name: string
+	phone: string
+	login: string
+	avatar: string
+	email: string
+}
+
+export class AuthApi {
+	private http: HTTPTransport
+
+	constructor () {
+		this.http = new HTTPTransport('/auth')
+	}
+
+	async signup (payload: SignupRequestData): Promise<SignupResponseData> {
+		return this.http.post('/signup', { data: payload })
+	}
+
+	signin (payload: SigninRequestData) {
+		return this.http.post('/signin', { data: payload })
+	}
+
+	async user (): Promise<UserInfoResponseData> {
+		return this.http.get('/user')
+	}
+
+	async logout (): Promise<void> {
+		return this.http.post('/logout')
+	}
+}
