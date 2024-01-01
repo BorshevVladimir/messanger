2f3e8ddc4a83f048d584c930c731550089bd0d26
Vladimir Borshev <herizon@yandex.ru>
2024-01-01 17:27:26 +0300

    Add withStore function

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,9 +1,33 @@
 import { EventBus } from '../utils/EventBus'
 import { set } from '../utils/set'
 import type { Indexed } from '../utils/isObject'
+import type { Block } from '../utils/Block'
+import { deepEqual } from '../utils/deepEqual'
 
 export enum StoreEvents {
 	Updated = 'updated',
+}
+
+export function withStore (mapStateToProps: (state: Indexed) => Indexed) {
+	return function connect (Component: typeof Block) {
+		return class extends Component {
+			constructor (props: Indexed) {
+
+				let prevState = mapStateToProps(store.getState())
+				super({ ...props, ...prevState })
+
+				store.on(StoreEvents.Updated, () => {
+					const newState = mapStateToProps(store.getState())
+
+					if (!deepEqual(prevState, newState)) {
+						this.setProps({ ...newState })
+					}
+
+					prevState = newState
+				})
+			}
+		}
+	}
 }
 
 class Store extends EventBus {
