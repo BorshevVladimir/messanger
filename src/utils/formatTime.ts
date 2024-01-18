8707650fd5250ef1cd9da76c3d29557778c1ff48
Борщев Владимир Андреевич <va.borshhev@rainvest.ru>
2024-01-18 23:07:47 +0300

    Add leading zeros to the formatTime function

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,10 +1,10 @@
 export function formatTime (time: string): string {
 	const date = new Date(time)
 	const year = date.getFullYear()
-	const month = date.getMonth() + 1
-	const day = date.getDay()
-	const hours = date.getHours()
-	const minutes = date.getMinutes()
+	const month = (date.getMonth() + 1).toString().padStart(2, '0')
+	const day = date.getDay().toString().padStart(2, '0')
+	const hours = date.getHours().toString().padStart(2, '0')
+	const minutes = date.getMinutes().toString().padStart(2, '0')
 
 	return `${hours}:${minutes} ${day}-${month}-${year}`
 }
