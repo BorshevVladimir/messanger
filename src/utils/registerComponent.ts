4dcdd079fe85975db3431f60bbed4a4c32f6d179
Борщев Владимир Андреевич <va.borshhev@rainvest.ru>
2024-01-23 16:49:53 +0300

    Fix ts errors in registerComponent unit

--- src/utils/registerComponent.ts
+++ src/utils/registerComponent.ts
@@ -1,7 +1,7 @@
 import { Block } from './Block'
 import Handlebars, { type HelperOptions } from 'handlebars'
 
-export function registerComponent (name: string, Component: typeof Block) {
+export function registerComponent <TComponent extends typeof Block<any>> (name: string, Component: TComponent) {
 	if (name in Handlebars.helpers) {
 		throw new Error(`Компонент ${name} уже зарегистрирован`)
 	}
